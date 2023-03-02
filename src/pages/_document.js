c4fb7c03cb0f721c07b49b5a73fc1a75e0cf4551
Antoine Delcourte <adelcourte.pro@gmail.com>
2023-03-02 09:57:45 +0100

    Web Keeper -> Larry

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head title="Larry, your personnal web assistant" />
             <body>
                 <Main />
                 <NextScript />
