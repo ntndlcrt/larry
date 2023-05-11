0f43ab4e21abd7a20e211071c5d99f7374aaf056
iamdjoky <maude2413@hotmail.fr>
2023-05-11 16:33:23 +0200

    modif dispo collections

--- src/components/Card-Collections/card-collections.js
+++ src/components/Card-Collections/card-collections.js
@@ -14,7 +14,9 @@ function CardCollections ({ title, comment,privacyStatus, date, image,props}) {
   }
     return (
       <div className="flex flex-row justify-between p-3" onClick={handleClick}>
-        <div>
+        <img src={image} className='w-20 h-20'/>
+
+        <div className='pl-4'>
           <h2 className='text-3xl'>{title}</h2>
           <p className='text-xl'>{comment}</p>
           <p className='text-gray-300 text-xs'>{date}</p>
@@ -23,7 +25,6 @@ function CardCollections ({ title, comment,privacyStatus, date, image,props}) {
             </div>
             <hr className='w-72'/>
         </div>
-          <img src={image} className='w-20 h-20 rounded-full'/>
       </div>
     );
   }
